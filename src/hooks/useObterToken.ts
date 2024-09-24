70597ad04eb5c07b59c44c933e367dba8d83daa2
Leonardo Luz <leonardoluz10@hotmail.com>
2024-09-24 09:39:47 -0300

    feat: :white_check_mark: Recebendo dados de pedidos pela API passando o token.

--- src/hooks/useObterToken.ts
+++ src/hooks/useObterToken.ts
@@ -1,5 +1,5 @@
 function useObterToken() {
-  return ()=> {
+  return () => {
     return sessionStorage.getItem("token");
   };
 }
--- src/paginas/Pedidos/index.tsx
+++ src/paginas/Pedidos/index.tsx
@@ -1,48 +1,45 @@
 import { AbBotao } from "ds-alurabooks";
 import "./Pedidos.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import useObterToken from "../../hooks/useObterToken";
+import { IPedido } from "../../interfaces/IPedido";
 
 const Pedidos = () => {
-    return(
-        <section className="secaoPedidos">
-            <h1 className="secaoPedidos-titulo">Pedidos</h1>
-            <div className="secaoPedidos__containerPedido">
-                <ul>
-                    <li>Pedido: <strong>123456</strong></li>
-                    <li>Data do pedido: <strong>19/09/2024</strong></li>
-                    <li>Valor total: <strong>R$ 42.00</strong></li>
-                    <li>Entrega realizada em: <strong>23/09/2024</strong></li>
-                </ul>
-                <AbBotao texto="Detalhes"/>
-            </div>
-            <div className="secaoPedidos__containerPedido">
-                <ul>
-                    <li>Pedido: <strong>123456</strong></li>
-                    <li>Data do pedido: <strong>19/09/2024</strong></li>
-                    <li>Valor total: <strong>R$ 42.00</strong></li>
-                    <li>Entrega realizada em: <strong>23/09/2024</strong></li>
-                </ul>
-                <AbBotao texto="Detalhes"/>
-            </div>
-            <div className="secaoPedidos__containerPedido">
-                <ul>
-                    <li>Pedido: <strong>123456</strong></li>
-                    <li>Data do pedido: <strong>19/09/2024</strong></li>
-                    <li>Valor total: <strong>R$ 42.00</strong></li>
-                    <li>Entrega realizada em: <strong>23/09/2024</strong></li>
-                </ul>
-                <AbBotao texto="Detalhes"/>
-            </div>
-            <div className="secaoPedidos__containerPedido">
-                <ul>
-                    <li>Pedido: <strong>123456</strong></li>
-                    <li>Data do pedido: <strong>19/09/2024</strong></li>
-                    <li>Valor total: <strong>R$ 42.00</strong></li>
-                    <li>Entrega realizada em: <strong>23/09/2024</strong></li>
-                </ul>
-                <AbBotao texto="Detalhes"/>
-            </div>
-        </section>
-    )
+  const [pedidos, setPedidos] = useState<IPedido[]>([]);
+  const token = useObterToken();
+  const formatador = Intl.NumberFormat('pt-br', {
+    style: "currency",
+    currency: "BRL"
+  })
+
+  useEffect(() => {
+    axios.get<IPedido[]>('http://localhost:8000/pedidos', {
+      headers: {
+        "Authorization": `Bearer ${token()}`
+      }
+    })
+      .then(resp => setPedidos(resp.data))
+      .catch(error => console.log(error))
+  }, [])
+
+  return (
+    <section className="secaoPedidos">
+      <h1 className="secaoPedidos-titulo">Pedidos</h1>
+      {pedidos.map(pedido => (
+        <div className="secaoPedidos__containerPedido" key={pedido.id}>
+          <ul>
+            <li>Pedido: <strong>{pedido.id}</strong></li>
+            <li>Data do pedido: <strong>{new Date(pedido.data).toLocaleDateString()}</strong></li>
+            <li>Valor total: <strong>{formatador.format(pedido.total)}
+            </strong></li>
+            <li>Entrega realizada em: <strong>{new Date(pedido.entrega).toLocaleDateString()}</strong></li>
+          </ul>
+          <AbBotao texto="Detalhes" />
+        </div>
+      ))}
+    </section>
+  )
 }
 
 export default Pedidos;
